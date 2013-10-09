e5f33dd5c601a3da3b796e3e86ad479c54fb06a2
jgacas <jugoslav.gacas@gmail.com>
2013-10-09 13:40:49 +0200

    changed migration 'create_users' so password column now has maximum lenght (note: migrations should not be changed, but new one should be added)

--- app/database/migrations/2013_10_09_085518_create_users.php
+++ app/database/migrations/2013_10_09_085518_create_users.php
@@ -18,7 +18,7 @@ class CreateUsers extends Migration {
 			$table->increments('id');
 			$table->string('first_name', 40);
 			$table->string('last_name', 40);
-			$table->string('password', 40);
+			$table->string('password');
 			$table->string('email');
 			$table->timestamps();
 		});
