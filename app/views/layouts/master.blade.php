da288cf5c2c1d0ebf668a9b7e6dbc1c6c01cf97c
jgacas <jugoslav.gacas@gmail.com>
2013-10-10 14:48:20 +0200

    added default CSS file

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -5,20 +5,26 @@
 <head>
 	<meta charset="UTF-8">
 	@yield('meta_info')
+	
 	<title>{{ Lang::get('messages.project_name') }}</title>
+	
+	<!-- load default CSS file -->
+	{{ HTML::style('style.css') }}
 	@yield('scripts')
 </head>
 <body>
-	<div id='header'>
-		@include('header')
-	</div>
-	
-	<div id='content'>
-		@yield('content')
-	</div>
-	
-	<div id='footer'>
-		@include('footer')
+	<div id='container'>
+		<div id='header'>
+			@include('header')
+		</div>
+		
+		<div id='content'>
+			@yield('content')
+		</div>
+		
+		<div id='footer'>
+			@include('footer')
+		</div>
 	</div>
 </body>
 </html>
