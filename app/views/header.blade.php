6cf432ae1fb3c48e6c832e978e826658ca249b8c
jgacas <jugoslav.gacas@gmail.com>
2013-10-10 11:48:37 +0200

    removed comma from header

--- app/views/header.blade.php
+++ app/views/header.blade.php
@@ -3,7 +3,7 @@
 <div class="header">
 	<h1>ToDo Manager</h1>
 	<p>
-		{{ Auth::user()->first_name }}, 
+		{{ Auth::user()->first_name }}
 		{{ Auth::user()->last_name }}
 		{{ HTML::link('logout', 'Logout') }}
 	</p>
