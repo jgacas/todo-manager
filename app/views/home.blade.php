7f9e4a4f91f813fda36ee8bfbc025d3e42c60eae
jgacas <jugoslav.gacas@gmail.com>
2013-10-10 11:46:44 +0200

    added header and footer to the views changed home page to use header and footer

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -1,12 +1,16 @@
 <!-- app/views/home.blade.php -->
 
-<div class="header">
-	Welcome back, {{ Auth::user()->first_name }}
-	{{ HTML::link('logout', 'Logout') }}
-</div>
-
-
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>ToDo Manager</title>
+</head>
+<body>
 <div class="content">
-	<h1>ToDO Manager</h1>
+	@include('header')
 	<p>Future version of ToDo Manager will have task list here</p>
+	@include('footer')
 </div>
+</body>
+</html>
