903681c501e552aaee62a02969abb473ba0c7d70
jgacas <jugoslav.gacas@gmail.com>
2013-10-10 16:48:10 +0200

    added 'bootstrap tweeter' to the project renamed style/ folder to css/ renamed header template to oldheader (we will have new one with navbar)

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -1,20 +1,23 @@
+
 <!-- app/views/layouts/master.blade.php -->
 
 <!doctype html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
+	<meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
 	@yield('meta_info')
 	
 	<title>{{ Lang::get('messages.project_name') }}</title>
 	
 	<!-- load default CSS file -->
-	{{ HTML::style('style/style.css') }}
+	{{ HTML::style('css/style.css') }}
+	{{ HTML::style('css/bootsrap.css') }}
+	{{ HTML::style('css/bootstrap-responsive.css') }}
 </head>
 <body>
 	<div id='container'>
 		<div id='header'>
-			@include('header')
+			@include('oldheader')
 		</div>
 		
 		<div id='content'>
@@ -25,8 +28,10 @@
 			@include('footer')
 		</div>
 	</div>
+	
 	<!-- load script files -->
 	{{ HTML::script('js/jquery-1.10.2.min.js') }}
+	{{ HTML::script('js/bootstrap.min.js') }}
 	{{ HTML::script('js/script.js') }}
 	@yield('scripts')
 </body>
