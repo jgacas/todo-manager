fea02086d6c8604e711dcc4c752f9c91bf312e33
jgacas <jugoslav.gacas@gmail.com>
2013-10-12 21:18:41 +0200

    validation errors are displayed beside form fields in login form added sublime text 2 project files to gitignore

--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -6,23 +6,32 @@
 {{ Form::open(array('action' => 'LoginController@processLogin')) }}
 
 	<h2>Login</h2>
-	<!-- validator errors -->
-	<p>{{ $errors->first('email') }}</p>
-	<p>{{ $errors->first('password') }}</p>
 	
+	<div class="control-group-error">
 	@if (Session::has('login_errors'))
-		<span class="error">{{ Lang::get('messages.login_error') }}</span>
+		<span class="help-inline">{{ Lang::get('messages.login_error') }}</span>
 	@endif
+	</div>
 
 	<!-- username -->
 	<div class="control-group">
 		{{ Form::label('email', Lang::get('messages.email_address'), array('class' => 'control-label') ) }}
-		{{ Form::email('email') }}
+		<div class="controls">
+			{{ Form::email('email') }}
+			<span class="help-inline">
+				{{ $errors->first('email') }}
+			</span>
+		</div>
 	</div>
 	<!-- password -->
 	<div class="control-group">	
 		{{ Form::label('password', Lang::get('messages.password', array('class' => 'control-label'))) }}
-		{{ Form::password('password') }}
+		<div class="controls">	
+			{{ Form::password('password') }}
+			<span class="help-inline">
+				{{ $errors->first('password') }}
+			</span>
+		</div>
 	</div>
 	<!-- submit -->
 	<div class="control-group">
